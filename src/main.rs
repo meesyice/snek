4e9b252512b9830ff5545d3f6fe5877070f618f9
Mohammed Al-Laktah <meesy.ice@icloud.com>
2021-12-12 22:42:14 +0100

    we have a moving square!

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,14 @@ extern crate piston;
 
 use glutin_window::GlutinWindow;
 use opengl_graphics::{GlGraphics, OpenGL};
-use piston::event_loop::{EventSettings, Events};
-use piston::input::{RenderArgs, RenderEvent, UpdateArgs, UpdateEvent};
+use piston::event_loop::*;
+use piston::input::*;
 use piston::window::WindowSettings;
+
 
 struct Game {
     gl: GlGraphics,
+    snake: Snake,
 }
 
 impl Game {
@@ -19,7 +21,62 @@ impl Game {
 
         self.gl.draw(arg.viewport(), |_c, gl| {
             graphics::clear(black, gl);
-        })
+        });
+
+        self.snake.render(&mut self.gl, arg);
+    }
+
+    fn update(&mut self) {
+        self.snake.update();
+    }
+
+    fn pressed(&mut self, btn: &Button) {
+        let last_direction = self.snake.dir.clone();
+        self.snake.dir = match btn {
+            &Button::Keyboard(Key::Up) if last_direction != Direction::Down => Direction::Up,
+            &Button::Keyboard(Key::Left) if last_direction != Direction::Right => Direction::Left,
+            &Button::Keyboard(Key::Right) if last_direction != Direction::Left => Direction::Right,
+            &Button::Keyboard(Key::Down) if last_direction != Direction::Up => Direction::Down,
+            _ => last_direction,
+        };
+    }
+}
+
+#[derive(Clone, PartialEq)]
+enum Direction {
+    Up,
+    Left,
+    Right,
+    Down,
+}
+
+struct Snake {
+    pos_x: i32,
+    pos_y: i32,
+    dir: Direction,
+}
+
+impl Snake {
+    fn render(&self, gl: &mut GlGraphics, args: &RenderArgs) {
+        let blue: [f32; 4] = [0.0, 0.0, 1.0, 1.0];
+
+        let square =
+            graphics::rectangle::square((self.pos_x * 20) as f64, (self.pos_y * 20) as f64, 20_f64);
+
+        gl.draw(args.viewport(), |c, gl| {
+            let transform = c.transform;
+
+            graphics::rectangle(blue, square, transform, gl)
+        });
+    }
+
+    fn update(&mut self) {
+        match self.dir {
+            Direction::Up => self.pos_y -= 1,
+            Direction::Left => self.pos_x -= 1,
+            Direction::Right => self.pos_x += 1,
+            Direction::Down => self.pos_y += 1,
+        }
     }
 }
 
@@ -34,12 +91,28 @@ fn main() {
 
     let mut game = Game {
         gl: GlGraphics::new(opengl),
+        snake: Snake {
+            pos_x: 4,
+            pos_y: 4,
+            dir: Direction::Up,
+        },
     };
 
-    let mut events = Events::new(EventSettings::new());
+    let mut events = Events::new(EventSettings::new()).ups(10);
+
     while let Some(e) = events.next(&mut window) {
         if let Some(r) = e.render_args() {
             game.render(&r);
+        }
+
+        if let Some(u) = e.update_args() {
+            game.update();
+        }
+
+        if let Some(k) = e.button_args() {
+            if k.state == ButtonState::Press {
+                game.pressed(&k.button);
+            }
         }
     }
 }
