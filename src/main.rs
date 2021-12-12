10e64245afc65eff9cd525e4ec078b86d5abc3bb
Mohammed Al-Laktah <meesy.ice@icloud.com>
2021-12-12 22:00:07 +0100

    We have a black box!

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,45 @@
+extern crate glutin_window;
+extern crate graphics;
+extern crate opengl_graphics;
+extern crate piston;
+
+use glutin_window::GlutinWindow;
+use opengl_graphics::{GlGraphics, OpenGL};
+use piston::event_loop::{EventSettings, Events};
+use piston::input::{RenderArgs, RenderEvent, UpdateArgs, UpdateEvent};
+use piston::window::WindowSettings;
+
+struct Game {
+    gl: GlGraphics,
+}
+
+impl Game {
+    fn render(&mut self, arg: &RenderArgs) {
+        let black: [f32; 4] = [0.0; 4];
+
+        self.gl.draw(arg.viewport(), |_c, gl| {
+            graphics::clear(black, gl);
+        })
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let opengl = OpenGL::V3_2;
+
+    let mut window: GlutinWindow = WindowSettings::new("Snek Gaem", [200, 200])
+        .graphics_api(opengl)
+        .exit_on_esc(true)
+        .build()
+        .unwrap();
+
+    let mut game = Game {
+        gl: GlGraphics::new(opengl),
+    };
+
+    let mut events = Events::new(EventSettings::new());
+    while let Some(e) = events.next(&mut window) {
+        if let Some(r) = e.render_args() {
+            game.render(&r);
+        }
+    }
 }
